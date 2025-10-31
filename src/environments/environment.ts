c75ead25b10b2b413ec8892054267e22b4fdb085
jesus alejandro aguilar valenzuela <jesualejandro17@gmail.com>
2025-10-31 09:02:49 -0500

    Remove environment files from repository

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://auphwijfzeojutlyjtgb.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1cGh3aWpmemVvanV0bHlqdGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MDExNjksImV4cCI6MjA3NzE3NzE2OX0.xq4Z6pML8DaY4x_KLUiK_zleiNrY-ujgEqATdVBH2Lo'
-};
